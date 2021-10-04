f9644f9c71b3888bfc77796abcc04c795944c4df
Konrad Kowalski <konrad.kowalski1991@o2.pl>
2021-10-04 21:55:35 +0200

    In Memory Repository

---
+++ XLibrary.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using XLibrary.Core.Models;
+
+namespace XLibrary.DataAccess.InMemory
+{
+    public class BookRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Book> books;
+
+        public BookRepository()
+        {
+            books = cache["books"] as List<Book>;
+            if (books == null)
+            {
+                books = new List<Book>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["books"] = books;
+        }
+
+        public void Insert(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void Update(Book book)
+        {
+            Book bookToUpdate = books.Find(b => b.Id == book.Id);
+
+            if (bookToUpdate != null)
+            {
+                bookToUpdate = book;
+            }
+            else
+            {
+                throw new Exception("Book not found");
+            }
+        }
+
+        public Book Find(string Id)
+        {
+            Book book = books.Find(b => b.Id == Id);
+
+            if (book != null)
+            {
+                return book;
+            }
+            else
+            {
+                throw new Exception("Book not found");
+            }
+        }
+
+        public IQueryable<Book> Collection()
+        {
+            return books.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Book bookToDelete = books.Find(b => b.Id == Id);
+
+            if (bookToDelete != null)
+            {
+                books.Remove(bookToDelete);
+            }
+            else
+            {
+                throw new Exception("Product no found");
+            }
+        }
+    }
+}
