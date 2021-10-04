87cf9461be4781c266e971c9c0fd36ecafb77c5c
Konrad Kowalski <konrad.kowalski1991@o2.pl>
2021-10-05 01:15:02 +0200

    Adding and editing books

--- XLibrary.Core/Models/Book.cs
+++ XLibrary.Core/Models/Book.cs
@@ -12,12 +12,14 @@ namespace XLibrary.Core.Models
     {
         public string Id { get; set; }
         public string Title { get; set; }
+        public string Author { get; set; }
         public string Publisher { get; set; }
         [DisplayName ("PublicationYear Year")]
         public int PublicationYear { get; set; }
         public string Description { get; set; }
         [DisplayName ("Is avaible")]
         public bool IsAvaible { get; set; }
+        public string Image { get; set; }
 
         public Book()
         {
---
+++ XLibrary.WebUI/Controllers/BookManagerController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XLibrary.Core.Models;
+using XLibrary.DataAccess.InMemory;
+
+
+namespace XLibrary.WebUI.Controllers
+{
+    public class BookManagerController : Controller
+    {
+        BookRepository context;
+
+        public BookManagerController()
+        {
+            context = new BookRepository();
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Book> books = context.Collection().ToList();
+            return View(books);
+        }
+
+        public ActionResult Create()
+        {
+            Book book = new Book();
+            return View(book);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(book);
+            }
+            else
+            {
+                context.Insert(book);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            Book book = context.Find(Id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(book);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Book book, string Id)
+        {
+            Book bookToEdit = context.Find(Id);
+
+            if (bookToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(book);
+                }
+
+                bookToEdit.Title = book.Title;
+                bookToEdit.Author = book.Author;
+                bookToEdit.Publisher = book.Publisher;
+                bookToEdit.PublicationYear = book.PublicationYear;
+                bookToEdit.Description = book.Description;
+                bookToEdit.IsAvaible = book.IsAvaible;
+                bookToEdit.Image = book.Image;
+
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            Book bookToDelete = context.Find(Id);
+
+            if (bookToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(bookToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Book bookToDelete = context.Find(Id);
+
+            if (bookToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
