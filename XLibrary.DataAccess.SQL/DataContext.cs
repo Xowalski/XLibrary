821991b1a70537b285d31600c69173a93e7a7b50
Konrad Kowalski <konrad.kowalski1991@o2.pl>
2021-10-05 19:35:10 +0200

    Entity Framework and migrations

---
+++ XLibrary.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XLibrary.Core.Models;
+
+namespace XLibrary.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ XLibrary.DataAccess.SQL/Migrations/202110051729292_Initial.cs
@@ -0,0 +1,33 @@
+﻿namespace XLibrary.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Title = c.String(),
+                        Author = c.String(),
+                        Publisher = c.String(),
+                        PublicationYear = c.Int(nullable: false),
+                        Description = c.String(),
+                        IsAvaible = c.Boolean(nullable: false),
+                        Image = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Books");
+        }
+    }
+}
