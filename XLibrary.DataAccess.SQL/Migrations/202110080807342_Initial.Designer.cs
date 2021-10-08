bf1ffbb536ffa51a447f7d9153b7c5781f8caac6
Konrad Kowalski <konrad.kowalski1991@o2.pl>
2021-10-08 11:16:09 +0200

    Reservation system

--- XLibrary.DataAccess.SQL/Migrations/202110080807342_Initial.Designer.cs
+++ XLibrary.DataAccess.SQL/Migrations/202110080807342_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace XLibrary.DataAccess.SQL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class AddReaders : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AddReaders));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "202110061854200_AddReaders"; }
+            get { return "202110080807342_Initial"; }
         }
         
         string IMigrationMetadata.Source
---
+++ XLibrary.DataAccess.SQL/Migrations/202110080807342_Initial.cs
@@ -0,0 +1,63 @@
+﻿namespace XLibrary.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Title = c.String(),
+                        Author = c.String(),
+                        Publisher = c.String(),
+                        PublicationYear = c.Int(nullable: false),
+                        Description = c.String(),
+                        IsAvailable = c.Boolean(nullable: false),
+                        Image = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Readers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(),
+                        FirstName = c.String(),
+                        Surname = c.String(),
+                        Email = c.String(),
+                        Street = c.String(),
+                        ZipCode = c.String(),
+                        City = c.String(),
+                        Country = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Reservations",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        ReservedBookId = c.String(),
+                        ReaderId = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Reservations");
+            DropTable("dbo.Readers");
+            DropTable("dbo.Books");
+        }
+    }
+}
